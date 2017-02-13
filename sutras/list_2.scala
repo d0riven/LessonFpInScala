5e0f11db7600f767cd373265ec8b0677431e9791
vg-niitsuma <yuki_niitsuma@voyagegroup.com>
2017-02-14 00:27:17 +0900

    Add chapter 2 lesson code

---
+++ sutras/list_2.scala
@@ -0,0 +1,42 @@
+object MyModule {
+  def abs(n:Int):Int =
+    if(n < 0) -n
+    else n
+
+  def factorial(n:Int):Int = {
+    def go(n:Int, acc:Int):Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+    go(n, 1)
+  }
+
+  private def formatAbs(x:Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  private def formatFactorial(n:Int) = {
+    val msg = "The factorial of %d is %d."
+    msg.format(n, factorial(n))
+  }
+
+  def formatResult(name:String, n:Int, f:Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
+  def findFirst[A](as:Array[A], p:A => Boolean):Int = {
+    @annotation.tailrec
+    def loop(n:Int):Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  def main(args:Array[String]):Unit = {
+    println(formatAbs(-42))
+    println(formatFactorial(7))
+  }
+}
