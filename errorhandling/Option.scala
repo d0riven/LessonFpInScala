378cfb9f2571948ae3969b89c39185d1933f90b5
vg-niitsuma <yuki_niitsuma@voyagegroup.com>
2017-04-11 09:04:16 +0900

    Ex4.2 end

--- errorhandling/Option.scala
+++ errorhandling/Option.scala
@@ -1,3 +1,31 @@
+//sealed trait Option[+A]
+trait Option[+A] {
+  def map[B](f:A => B):Option[B] = this match {
+    case Some(x) => Some(f(x))
+    case None => None
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(x) => x
+    case None => default
+  }
+
+  def flatMap[B](f:A => Option[B]):Option[B] =
+    map(f) getOrElse None
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    this map (Some(_)) getOrElse ob
+
+  def filter(f:A => Boolean): Option[A] = this match {
+    case Some(a) if f(a) => this
+    case _ => None
+  }
+}
+case class Some[+A](get:A) extends Option[A]
+case object None extends Option[Nothing]
+
+case class Employee(name:String, department:String)
+
 object Option {
   def failingFn(i:Int):Int = {
     val y:Int = throw new Exception("fail!")
@@ -7,10 +35,28 @@ object Option {
     }
     catch { case e:Exception => 43 }
   }
+
+  def mean(xs:Seq[Double]):Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs:Seq[Double]):Option[Double] = {
+    //val m = mean(xs)
+    mean(xs) flatMap (m => mean(xs.map(s => math.pow(m - s, 2))))
+  }
 }
 
 object ExecOption {
+  def lookupByName(name:String):Option[Employee] = ???
+  /**
+   * Ex4.2
+   * @問題文
+   * flatMapをベースとしてvariance関数を実装せよ。シーケンスの平均をm、
+   * シーケンスの各要素をxとすれば、分散はmath.pow(x - m, 2)の平均となる。
+   */
   def main(args:Array[String]):Unit = {
-    println(Option.failingFn(1))
+    //println(Option.failingFn(1))
+    //val joeDepartment:Option[String] = lookupByName("Joe").map.(_.department)
+    println(Option.variance(Array(1, 2, 3, 4, 5)))
   }
 }
